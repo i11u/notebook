2c1cc3c05af3b13e84a5f8004691b1bafc7e9b6d
teru <incredibleteru@gmail.com>
2022-01-12 02:27:28 +0900

    Update Block, Page, and Document

--- renderer/types/Document.ts
+++ renderer/types/Document.ts
@@ -1,4 +1,6 @@
-type CaretLocation = {
+import { Page } from './Page';
+
+export type CaretLocation = {
   pageId: number;
   blockId: number;
   indexInBlock: number;
@@ -9,4 +11,14 @@ export type Caret = {};
 export type Document = {
   id: number;
   caretLocation: CaretLocation;
+  pages: Page[];
+};
+
+export const NewDocument = (): Document => {
+  const document: Document = {
+    id: 1,
+    caretLocation: { pageId: 0, blockId: 0, indexInBlock: 0 },
+    pages: [],
+  };
+  return document;
 };
